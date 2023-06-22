6dd0a6869d7d82a6607fe2d1b18a7ef33dbd818a
Kuper <killkih@mail.ru>
2023-06-22 20:00:00 +0300

    Lesson 20

--- lib/simpler/router/route.rb
+++ lib/simpler/router/route.rb
@@ -2,19 +2,32 @@ module Simpler
   class Router
     class Route
 
-      attr_reader :controller, :action
+      attr_reader :controller, :action, :params
 
       def initialize(method, path, controller, action)
         @method = method
         @path = path
         @controller = controller
         @action = action
+        @params = {}
       end
 
       def match?(method, path)
-        @method == method && path.match(@path)
+        matched_path = path.match(regexp_path(@path))
+
+        if @method == method && matched_path
+          @params = matched_path.named_captures
+          true
+        else
+          false
+        end
       end
 
+      private
+
+      def regexp_path(path)
+        Regexp.new("\\A#{path.gsub(/(:\w+)/, '(?<\1>\w+)').delete(':')}\\Z")
+      end
     end
   end
 end
